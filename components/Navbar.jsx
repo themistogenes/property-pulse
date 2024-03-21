7cd63111dee807b198e38f284ff56be8a0750b7b
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-03-21 17:31:03 -0400

    sign out and protect routes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -181,6 +181,9 @@ const Navbar = () => {
                     role="menuitem"
                     tabIndex="-1"
                     id="user-menu-item-0"
+                    onClick={() => {
+                      setIsProfileMenuOpen(false)
+                    }}
                   >
                     Your Profile
                   </Link>
@@ -190,10 +193,17 @@ const Navbar = () => {
                     role="menuitem"
                     tabIndex="-1"
                     id="user-menu-item-2"
+                    onClick={() => {
+                      setIsProfileMenuOpen(false)
+                    }}
                   >
                     Saved Properties
                   </Link>
                   <button
+                    onClick={() => {
+                      setIsProfileMenuOpen(false);
+                      signOut();
+                    }}
                     className="block px-4 py-2 text-sm text-gray-700"
                     role="menuitem"
                     tabIndex="-1"
