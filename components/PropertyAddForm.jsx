f0f114bc6c786830254d2b2f99ca26a3450c345b
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-03-27 18:29:23 -0400

    add property POST API route

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -100,7 +100,7 @@ const PropertyAddForm = () => {
   };
 
   return mounted && (
-    <form>
+    <form action="/api/properties" method="POST" encType="multipart/form-data">
       <h2 className="text-3xl text-center font-semibold mb-6">
         Add Property
       </h2>
@@ -542,7 +542,7 @@ const PropertyAddForm = () => {
         <input
           type="text"
           id="seller_name"
-          name="seller_info.name."
+          name="seller_info.name"
           className="border rounded w-full py-2 px-3"
           placeholder="Name"
           value={fields.seller_info.name}
@@ -599,6 +599,7 @@ const PropertyAddForm = () => {
           className="border rounded w-full py-2 px-3"
           accept="image/*"
           multiple
+          required
           onChange={handleImageChange}
         />
       </div>
