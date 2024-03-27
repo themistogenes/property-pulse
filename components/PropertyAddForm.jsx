6ad29b008d1afc2ed7182c02904a9bf175840906
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-03-27 17:45:18 -0400

    PropertyAddForm input handlers

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -34,9 +34,70 @@ const PropertyAddForm = () => {
     setMounted(true);
   }, [])
 
-  const handleChange = () => {};
-  const handleAmenitiesChange = () => {};
-  const handleImageChange = () => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // Check if nested property
+    if (name.includes('.')) {
+      const [outerKey, innerKey] = name.split('.');
+      
+      setFields((prevFields) => ({
+        ...prevFields,
+        [outerKey]: {
+          ...prevFields[outerKey],
+          [innerKey]: value
+        }
+      }))
+    } else {
+      // For a property that is not nested
+      setFields((prevFields) => ({
+        ...prevFields,
+        [name]: value
+      }))
+    }
+
+  };
+  const handleAmenitiesChange = (e) => {
+    const { value, checked } = e.target;
+
+    // Clone the current amenities array
+    const updatedAmenities = [...fields.amenities];
+
+    if (checked) {
+      // Add value to array
+      updatedAmenities.push(value);
+    } else {
+      // Remove value from array;
+      const index = updatedAmenities.indexOf(value);
+
+      if (index !== -1) {
+        updatedAmenities.splice(index, 1);
+      }
+    }
+
+    // Update state with updated array
+    setFields((prevFields) => ({
+      ...prevFields,
+      amenities: updatedAmenities
+    }))
+  };
+  const handleImageChange = (e) => {
+    const { files } = e.target;
+    
+    // Clone images array
+    const updatedImages = [...fields.images];
+
+    // Add new files to the array
+    for (const file of files) {
+      updatedImages.push(file);
+    }
+
+    // Update state with array of images
+    setFields((prevFields) => ({
+      ...prevFields,
+      images: updatedImages
+    }))
+  };
 
   return mounted && (
     <form>
